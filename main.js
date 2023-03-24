125333e61d3302b9fb7d3fbf91ad93618f6650f6
DAVID CAMILO RODRIGUEZ SILVA <david.rodriguezs@aviatur.com.co>
2023-03-24 15:03:37 -0500

    Incorporacion del menu del carrito

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
-const navEmail= document.querySelector('.navbar-email');    
-const desktopmenu= document.querySelector('.desktop-menu');
-const menuDesplegable= document.querySelector('.menu');
-const mobilemenu= document.querySelector('.mobile-menu');
+const navEmail = document.querySelector('.navbar-email');    
+const desktopmenu = document.querySelector('.desktop-menu');
+const menuDesplegable = document.querySelector('.menu');
+const mobilemenu = document.querySelector('.mobile-menu');
+const iconcarrito = document.querySelector('.navbar-shopping-cart');
+const menucarrito = document.querySelector('.product-detail')
 
 navEmail.addEventListener('click', toggleDesktopMenu);
 menuDesplegable.addEventListener('click', toggleMobileMenu);
+iconcarrito.addEventListener('click', togglecarrito);
 
 function toggleDesktopMenu(){
+    mobilemenu.classList.add('inactive');
+    menucarrito.classList.add('inactive');
     desktopmenu.classList.toggle('inactive');    
 }
 function toggleMobileMenu(){
-    mobilemenu.classList.toggle('inactive');    
+    desktopmenu.classList.add('inactive');
+    menucarrito.classList.add('inactive');
+    mobilemenu.classList.toggle('inactive');  
+}
+function togglecarrito(){
+    desktopmenu.classList.add('inactive');
+    mobilemenu.classList.add('inactive');
+    menucarrito.classList.toggle('inactive');
 }
